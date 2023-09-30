3e22445f29b6ab27efe350d6cf3a9b9c132e5e10
viratkohli2011 <sgargalbany@gmail.com>
2023-09-29 23:50:03 -0400

    Dept Dictionary

---
+++ services/frontend/src/components/ShashankTest.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="dept in depts">
+      {{ dept.name }}
+      <ul v-for="course in dept.courses">
+        <li>{{ course }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  export default {
+        data() {
+            return {
+              depts: [{name: "CSCI", courses: ["CS1", "DS", "FOCS", "Comp_Org"]}, {name: "HASS", courses: ["H1", "H2", "H3", "H4"]}],
+              courses: []
+            };
+        },
+        methods: {
+
+        }
+  }
+</script>
+
+<style scoped>
+
+</style>
