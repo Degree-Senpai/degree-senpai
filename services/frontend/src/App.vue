e4716aa641e5f7e1c67c75d2fe8f56a0d8c0b9fd
EggyRepublic <Eggyrepublic@gmail.com>
2023-10-03 16:56:40 -0400

    script to make seo happy

--- services/frontend/src/App.vue
+++ services/frontend/src/App.vue
@@ -4,6 +4,36 @@
   </div>
 </template>
 
+<script>
+  export default {
+    name: 'DegreeSenpai',
+    components: [],
+    metaInfo() {
+      return {
+        title: "DegreeSenpai",
+        titleTemplate: null,
+        meta: [
+          {
+            vmid: "description",
+            name: "description",
+            content:
+              "Degree Planner and Course Scheduler for RPI Students",
+          },
+          {
+            vmid: "keywords",
+            content: "RPI, Degree Senpai, RPI Scheduler, Course Picker, Course Scheduler, Degree Planner, Rensselaer Polytechnic Institute",
+          },
+          { property: "og:title", content: "Degree Planner and Course Scheduler" },
+          { property: "og:site_name", content: "Degree Senpai" },
+          { property: "og:type", content: "website" },
+          { name: "robots", content: "index" },
+          { name: "color-scheme", content: this.colorSchemeMeta },
+        ],
+      };
+    }
+  }
+</script>
+
 <style>
   body {
     font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
