c68e4d9bdc9fce6ec4ce4e7c04be668a9d94007f
graya4 <graya4@rpi.edu>
2023-10-31 17:39:38 -0400

    now goes to subject page; still bugs/WIP

--- services/backend/src/scraper.py
+++ services/backend/src/scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 import time
 
 # Provide the URL of the webpage you want to access
@@ -36,19 +37,35 @@ def login(username, password):
         print("Exception occurred during execution:", e)
 
 #constantly checks to see if you're logged in yet
-while login(username, password) is False:
+while True:
+    if login(username, password) is True:
+        break
+    else:
+        try:
+            print("LOGIN PROCESS")
+            # Add your logic to handle the webpage changes here
+            # Example: Print the current URL every 5 seconds
+            print("Current URL:", driver.current_url)
+
+            # Sleep for 5 seconds before the next iteration
+            time.sleep(5)
+        except Exception as e:
+            print("Exception occurred during execution:", e)
+
+while True:
     try:
-        
         # Add your logic to handle the webpage changes here
         # Example: Print the current URL every 5 seconds
         print("Current URL:", driver.current_url)
-
+        duo_button = driver.find_element(By.ID, "trust-browser-button")
+        duo_button.click()
+        #time.sleep(5)
+        break
+        
         # Sleep for 5 seconds before the next iteration
-        time.sleep(5)
+        
     except Exception as e:
         print("Exception occurred during execution:", e)
-            
-
 #once youre logged in
 while True:
     try:
@@ -58,13 +75,15 @@ while True:
         student_menu = driver.find_element(By.LINK_TEXT, "Student Menu")
         print(student_menu)
         student_menu.click()
+        #time.sleep(5)
         break
         
         # Sleep for 5 seconds before the next iteration
-        time.sleep(5)
+        
     except Exception as e:
         print("Exception occurred during execution:", e)
 
+# finding class search
 while True:
     try:
         # Add your logic to handle the webpage changes here
@@ -72,13 +91,49 @@ while True:
         print("Current URL:", driver.current_url)
         class_search = driver.find_element(By.LINK_TEXT, "Class Search")
         class_search.click()
-        term_search = driver.find_element(By.NAME, "p_term")
-        term_search.click()
+        #time.sleep(5)
+        break
         # Sleep for 5 seconds before the next iteration
-        time.sleep(5)
+        
     except Exception as e:
         print("Exception occurred during execution:", e)
 
+# clicking on Spring 2024 tab
+while True:
+    try:
+        term_search = Select(driver.find_element(By.ID, "term_input_id"))
+        spring2024 = term_search.select_by_value("202401")
+        break
+        
+        spring2024.click()
+        #time.sleep(5)
+        
+        
+    except Exception as e:
+        print("Exception occurred during execution:", e)
 
+while True:
+    try:
+        print("CLICK THE FUCKING BUTTON")
+        submit_button = driver.find_element(By.XPATH, "/html/body/div[3]/form/input[2]")
+        submit_button.click()
+        #time.sleep(5)
+        break
+        
+    except Exception as e:
+        print("Exception occurred during execution:", e)
+
+subjectCount = 0
+while True:
+    try:
+        subjectList = Select(driver.find_element(By.XPATH,'//*[@id="subj_id"]'))
+        subjectList.select_by_index(subjectCount)
+        subjectButton = driver.find_element(By.XPATH, '//*[@id="advCourseBtnDiv"]/input[1]')
+        subjectButton.click()
+        time.sleep(10)
+        driver.back()
+        subjectCount += 1
+    except Exception as e:
+        print("Exception occurred during execution:", e)
 # Close the WebDriver
 driver.quit()
