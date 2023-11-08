87c64e50ca31f96b66e04f5ca8448db3aa768d79
EggyRepublic <Eggyrepublic@gmail.com>
2023-11-08 06:57:26 -0500

    I didn't know C++ syntax can be simplified like this
    but I'm glad I found out!
    
    Love one liners lmao

--- services/backend/src/scheduler/test.cpp
+++ services/backend/src/scheduler/test.cpp
@@ -8,74 +8,28 @@ int main() {
     datastructures1timeblocks.push_back(std::make_shared<TimeBlock>(1, 720, 830));
     datastructures1timeblocks.push_back(std::make_shared<TimeBlock>(4, 720, 830));
 
-    std::vector<int> datastructures1timeblocksnew = {2280, 2390, 6600, 6710};
-    // make course instance
-    std::shared_ptr<CourseInstance> datastructures1 = std::make_shared<CourseInstance>("data structures", 20001, datastructures1timeblocksnew);
+    std::shared_ptr<CourseInstance> datastructures1 = std::make_shared<CourseInstance>("data structures", 20001, std::vector<int>{2280, 2390, 6600, 6710});
 
-    // generate time blocks
-    std::vector<std::shared_ptr<TimeBlock>> cs1timeblocks;
-    cs1timeblocks.push_back(std::make_shared<TimeBlock>(0, 660, 770));
-    cs1timeblocks.push_back(std::make_shared<TimeBlock>(3, 660, 770));
+    std::shared_ptr<CourseInstance> cs1 = std::make_shared<CourseInstance>("computer science", 10001, std::vector<int>{840, 950, 5160, 5270});
+    std::shared_ptr<CourseInstance> cs2 = std::make_shared<CourseInstance>("computer science", 10002, std::vector<int>{2280, 2390, 6600, 6710});
 
-    std::vector<int> cs1timeblocksnew = {840, 950, 5160, 5270};
-    // make course instance
-    std::shared_ptr<CourseInstance> cs1 = std::make_shared<CourseInstance>("computer science", 10001, cs1timeblocksnew);
+    std::shared_ptr<CourseInstance> animation1 = std::make_shared<CourseInstance>("3D animation", 41001, std::vector<int>{840, 950, 5160, 5270});
+    std::shared_ptr<CourseInstance> animation2 = std::make_shared<CourseInstance>("3D animation", 41002, std::vector<int>{960, 1070, 5280, 5390});
+    std::shared_ptr<CourseInstance> animation3 = std::make_shared<CourseInstance>("3D animation", 41003, std::vector<int>{2280, 2390, 6600, 6710});
+    std::shared_ptr<CourseInstance> animation4 = std::make_shared<CourseInstance>("3D animation", 41004, std::vector<int>{2340, 2390, 6660, 6710});
 
-    // generate time blocks
-    std::vector<std::shared_ptr<TimeBlock>> cs2timeblocks;
-    cs2timeblocks.push_back(std::make_shared<TimeBlock>(1, 660, 770));
-    cs2timeblocks.push_back(std::make_shared<TimeBlock>(4, 660, 770));
+    std::shared_ptr<CourseInstance> graphic0 = std::make_shared<CourseInstance>("Graphics Storytelling", 42000, std::vector<int>{480, 590, 4800, 4910});
+    std::shared_ptr<CourseInstance> graphic1 = std::make_shared<CourseInstance>("Graphics Storytelling", 42001, std::vector<int>{840, 950, 5160, 5270});
+    std::shared_ptr<CourseInstance> graphic2 = std::make_shared<CourseInstance>("Graphics Storytelling", 42002, std::vector<int>{960, 1070, 5280, 5390});
+    std::shared_ptr<CourseInstance> graphic3 = std::make_shared<CourseInstance>("Graphics Storytelling", 42003, std::vector<int>{2280, 2390, 6600, 6710});
+    std::shared_ptr<CourseInstance> graphic4 = std::make_shared<CourseInstance>("Graphics Storytelling", 42004, std::vector<int>{2340, 2390, 6660, 6710});
 
-    std::vector<int> cs2timeblocksnew = {2280, 2390, 6600, 6710};
-    // make course instance
-    std::shared_ptr<CourseInstance> cs2 = std::make_shared<CourseInstance>("computer science", 10002, cs2timeblocksnew);
-
-    std::vector<int> animation1tb = {840, 950, 5160, 5270};
-    std::shared_ptr<CourseInstance> animation1 = std::make_shared<CourseInstance>("3D animation", 41001, animation1tb);
-    std::vector<int> animation2tb = {960, 1070, 5280, 5390};
-    std::shared_ptr<CourseInstance> animation2 = std::make_shared<CourseInstance>("3D animation", 41002, animation2tb);
-    std::vector<int> animation3tb = {2280, 2390, 6600, 6710};
-    std::shared_ptr<CourseInstance> animation3 = std::make_shared<CourseInstance>("3D animation", 41003, animation3tb);
-    std::vector<int> animation4tb = {2340, 2390, 6660, 6710};
-    std::shared_ptr<CourseInstance> animation4 = std::make_shared<CourseInstance>("3D animation", 41004, animation4tb);
-
-    std::vector<int> graphic0tb = {480, 590, 4800, 4910};
-    std::shared_ptr<CourseInstance> graphic0 = std::make_shared<CourseInstance>("Graphics Storytelling", 42000, graphic0tb);
-    std::vector<int> graphic1tb = {840, 950, 5160, 5270};
-    std::shared_ptr<CourseInstance> graphic1 = std::make_shared<CourseInstance>("Graphics Storytelling", 42001, graphic1tb);
-    std::vector<int> graphic2tb = {960, 1070, 5280, 5390};
-    std::shared_ptr<CourseInstance> graphic2 = std::make_shared<CourseInstance>("Graphics Storytelling", 42002, graphic2tb);
-    std::vector<int> graphic3tb = {2280, 2390, 6600, 6710};
-    std::shared_ptr<CourseInstance> graphic3 = std::make_shared<CourseInstance>("Graphics Storytelling", 42003, graphic3tb);
-    std::vector<int> graphic4tb = {2340, 2390, 6660, 6710};
-    std::shared_ptr<CourseInstance> graphic4 = std::make_shared<CourseInstance>("Graphics Storytelling", 42004, graphic4tb);
-
-    std::vector<std::vector<std::shared_ptr<CourseInstance>>> courseInstances;
-
-    std::vector<std::shared_ptr<CourseInstance>> datastructures;
-    datastructures.push_back(datastructures1);
-
-    std::vector<std::shared_ptr<CourseInstance>> cs;
-    cs.push_back(cs1);
-    cs.push_back(cs2);
-
-    std::vector<std::shared_ptr<CourseInstance>> animation;
-    animation.push_back(animation1);
-    animation.push_back(animation2);
-    animation.push_back(animation3);
-    animation.push_back(animation4);
-
-    std::vector<std::shared_ptr<CourseInstance>> graphic;
-    graphic.push_back(graphic0);
-    graphic.push_back(graphic1);
-    graphic.push_back(graphic2);
-    graphic.push_back(graphic3);
-    graphic.push_back(graphic4);
-
-    courseInstances.push_back(datastructures);
-    courseInstances.push_back(cs);
-    courseInstances.push_back(animation);
-    courseInstances.push_back(graphic);
+    std::vector<std::vector<std::shared_ptr<CourseInstance>>> courseInstances = {
+        {datastructures1},
+        {cs1, cs2},
+        {animation1, animation2, animation3, animation4},
+        {graphic0, graphic1, graphic2, graphic3, graphic4}
+    };
 
     scheduler.populate(courseInstances, 5);
 }
