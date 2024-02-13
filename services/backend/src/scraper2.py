4c0341fccd6de92fe7ead043a8e621d3e552fffd
russellchao <russchaoman@gmail.com>
2024-02-13 15:39:34 -0500

    Commit as of 2/13

--- services/backend/src/scraper2.py
+++ services/backend/src/scraper2.py
@@ -1,7 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+from datetime import datetime
 
-url = "C:/Users/chaor/degree-senpai/Search Results.html"
+## HELPER FUNCTION - Date Strong Conversion ##
+def convert_date(date):
+    # Assuming MM/DD format and a specific year
+    date_str = date  # Example date
+    specific_year = 2024 # Example year
+
+    # Combine the specific year with the MM/DD format
+    date_iso = f"{specific_year}-{date_str}"
+
+    # Convert to ISO 8601 format
+    date_obj = datetime.strptime(date_iso, "%Y-%m/%d")
+    iso_format = date_obj.isoformat()
+
+    return iso_format
+    # print(iso_format)  # Output will be in "YYYY-MM-DD" format for the specific year
+
+
+
+
+
+
+# url1 = "C:/Users/chaor/degree-senpai/Search Results.html"
+url = "C:/Users/chaor/degree-senpai/CompSciClasses.html"
 
 # Open and read the URL
 page = open(url)
@@ -42,9 +65,35 @@ for row in tables[5].find_all('tr'):
 
     # For the third and beyond rows, take the data (e.g. CRN, Meeting days and times, professor) from the cell 
     this_course = dict()
+    
+
+    # include the location, date, and instructor all in one label (this is to ensure that classes with multiple meeting times e.g. labs are accommodated with the same class)
+    this_course["Timeslots"] = []
+    
+
+    # Create the timeslot for this specific meeting
+    this_timeslot = []  
+
+
     for i in range(len(cell_data)):
         # Set the data in the cell to its respective label (e.g. CRN=93972, Time=08:00, Cap=1000, Instructor='Wes Turner')
         this_course[labels_list[i]] = cell_data[i]
+
+        # Add the days, time and location labels to the Timeslot label
+        if (labels_list[i] == 'Days' or labels_list[i] == 'Time' or labels_list[i] == 'Location'):
+            this_timeslot.append(cell_data[i])
+
+        # Convert the dates to ISO 8601 format then add the date to the Timeslot label
+        if (labels_list[i] =='Date (MM/DD)'):
+            dates = cell_data[i].split('-')
+            for d in dates:
+                dates = convert_date(d)
+            this_timeslot.append(dates)
+
+    this_course['Timeslots'].append(this_timeslot)
+        
+
+        
 
 
     # Add this course dictionary to the courses list (for rows 3+)
@@ -54,3 +103,4 @@ for row in tables[5].find_all('tr'):
 for c in courses_list:
     print(c)
     print()
+
